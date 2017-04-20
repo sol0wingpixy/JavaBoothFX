f7b8d8adeb8e160da5103ae70897d9703de319ef
Rhys Miller <miller5157r@ckstudents.org>
2017-04-20 09:54:31 -0700

    Start test application

--- src/jbfx/sample/Main.java
+++ src/jbfx/sample/Main.java
@@ -10,14 +10,30 @@ public class Main {
     public static void main(String[] args) {
         Sprite sprite = new Sprite();
         sprite.addFrameHandler(new FrameHandler() {
+            private double velX;
+            private double velY=1;
             @Override
             public void runPerTick(long now) {
-                sprite.translateX(1);
+                velX+=.1;
+                velY+=.01;
+                sprite.translateX(velX);
+                sprite.translateY(velY);
                 System.out.println("RunPerTick");
                 System.out.println(sprite.getNode().translateXProperty());
+                if(sprite.getNode().translateXProperty().greaterThan(1000).get())
+                {
+                    sprite.translateX(-1000);
+                }
+                if(sprite.getNode().translateYProperty().greaterThan(600).get())
+                {
+                    sprite.translateY(-800);
+                }
             }
         });
+
         Window.addSprite(sprite);
+        Window.setWidth(1000);
+        Window.setHeight(600);
         System.out.println("Launching Application");
         Application.launch(Window.class,args);
     }
--- src/jbfx/window/Window.java
+++ src/jbfx/window/Window.java
@@ -12,19 +12,23 @@ import java.util.ArrayList;
  * Created by miller5157r on 4/14/2017.
  */
 public class Window extends Application {
-    private static ArrayList<Sprite> sprites;
-    static {
-        sprites = new ArrayList<>();
-    }
+    private static ArrayList<Sprite> sprites = new ArrayList<>();
+    private static double width = 100;
+    private static double height = 100;
     public static void addSprite(Sprite sprite)
     {
         sprites.add(sprite);
     }
-
+    public static void setWidth(double widthIn){
+        width = widthIn;
+    }
+    public static void setHeight(double heightIn){
+        height = heightIn;
+    }
     public void start(Stage stage) {
         System.out.println("Starting Application");
         Group root = new Group();
-        Scene scene = new Scene(root);
+        Scene scene = new Scene(root,width,height);
         for(Sprite s:sprites)
         {
             System.out.println("Starting FrameHandler");
