9616848dc095a38d5cc75b2bc53cc0ac2023b448
Rhys Miller <miller5157r@ckstudents.org>
2017-05-31 10:54:09 -0700

    Commenting! Uses Javadoc method

--- src/jbfx/Sprite.java
+++ src/jbfx/Sprite.java
@@ -4,14 +4,27 @@ import javafx.scene.Node;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.shape.Rectangle;
-
+/**
+*This is the basic Sprite class. Everything that appears on screen will directly extend either Sprite or AnimatedSprites.\n
+* Each frame, the {@code runPerTick(long now)} method will be called.
+* Whenever a key is pressed, the {@code whenKeyPressed(KeyEvent event)} method is called, with {@code event} representing the proper key.
+* Whenever it collides with another Sprite, {@code collidesWith(Sprite other)} will be called for each sprite.\n
+ * All these methods must be overriden by the user's own Sprite.
+ */
 public abstract class Sprite {//to be implemented by user
+
     private Node node;
     private double heading;
     private Game parentGame;//needed for accessing KeyStates and camera
     private double offsetX = 0;
     private double offsetY = 0;
 
+    /**
+     * This constructor is used if a Sprite creates another Sprite.
+     * @param parent The Sprite that is creating this Sprite.
+     * @param inNode The Node that is sent in by user. Will be drawn. Is automatically moved such that the parent Sprite is the origin.
+     * @param heading Direction this Sprite faces by default, in degrees.
+     */
     public Sprite(Sprite parent,Node inNode,double heading)
     {
         node = inNode;
@@ -20,25 +33,47 @@ public abstract class Sprite {//to be implemented by user
         changeOffsetValues(parent.getOffsetX(),parent.getOffsetY());
     }
 
+    /**
+     * This constructor is used if a Sprite creates another Sprite. Has a default heading of 0.
+     * @param parent The Sprite that is creating this Sprite.
+     * @param inNode The Node that is sent in by user. Will be drawn. Is automatically moved such that the parent Sprite is the origin.
+     */
     public Sprite(Sprite parent,Node inNode)
     {
         this(parent,inNode,0);
     }
 
+    /**
+     * Simple constructor. Just sets internal values to the parameters.
+     * @param inNode The drawn node. Should not be offset prior to being added. Instead, make the Sprite, then use {@code move(x,y)}
+     * @param heading The direction the Node is facing in degrees. 0 = right, 90 = up, 180 = left, 270 = down. Will wrap around for values greater than 360. Supports negatives.
+     */
     public Sprite(Node inNode, double heading) {
         node = inNode;
         this.heading = heading;
     }
 
+    /**
+     * Simple constructor. Just sets internal values to the parameters. Defaults heading to 0.
+     * @param inNode The drawn node. Should not be offset prior to being added. Instead, make the Sprite, then use {@code move(x,y)}
+     */
     public Sprite(Node inNode) {
         this(inNode,0);
     }
 
+    /**
+     * Defaults Sprite to a black 10x10 rectangle, and heading to 0.
+     */
     public Sprite() {
         this(new Rectangle(10,10));
     }
 
     //ABSTRACT METHODS
+
+    /**
+     * 
+     * @param now
+     */
     public abstract void runPerTick(long now);//executed 60 times a second by Game
 
     public abstract void whenKeyPressed(KeyEvent event);//whenever a key is pressed
